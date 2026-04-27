23c03389bc06f6d78248f64ebea5e0a452dd7522
Saeed Mohamed <saeedmohamed.fs@gmail.com>
2026-04-27 17:52:36 +0300

    cmon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,41 @@ const pixelFont = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://sae8d-not-google.hf.space'),
+  metadataBase: new URL('https://msh-google.vercel.app'),
   title: 'Not Google - مش جوجل',
   description: 'IR Practical Project Implementation 2026',
-  icons: {
-    icon: '/g.svg',
-  },
   openGraph: {
     title: 'Not Google - مش جوجل',
     description: 'IR Practical Project Implementation 2026',
+    url: 'https://msh-google.vercel.app',
+    siteName: 'Not Google - مش جوجل',
     images: [
       {
         url: '/og.jpg',
         width: 1200,
         height: 630,
-        alt: 'Not Google - Retro Search',
+        alt: 'Not Google - مش جوجل',
       },
     ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Not Google - مش جوجل',
+    description: 'IR Practical Project Implementation 2026',
+    images: ['/og.jpg'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
 };
 
